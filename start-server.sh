52871ad173500207b04f4fe4eba763776585fc89
Min Min Latt <minminlaxz@gmail.com>
2024-06-05 00:00:07 +0630

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -2,5 +2,5 @@ docker run -d \
 --name xray \
 -p 1443:1443 \
 --restart=unless-stopped \
--v $(pwd)/config.json:/etc/xray/config.json \
+-v $(pwd)/server-config.json:/etc/xray/config.json \
 teddysun/xray
