ccdb1e2a69f9a01de272023ed7ad655b3b34216d
Min Min Latt <minminlaxz@gmail.com>
2024-06-11 21:21:11 +0630

    Update setup.sh

--- sing-box/server/setup.sh
+++ sing-box/server/setup.sh
@@ -1,1 +1,2 @@
-docker run --rm  ghcr.io/sagernet/sing-box generate rand --base64 16 && generate reality-keypair
+docker run --rm  ghcr.io/sagernet/sing-box generate rand --base64 16
+docker run --rm  ghcr.io/sagernet/sing-box generate reality-keypair
