6227186fe1220db8f29293f43beb1b461db06f0d
minlaxz <minminlaxz@gmail.com>
2025-11-05 17:53:56 +0630

    feat: Config server update, it's short but ugly

--- singbox/v2/config_server.py
+++ singbox/v2/config_server.py
@@ -28,17 +28,18 @@ class Handler(http.server.BaseHTTPRequestHandler):
         help_msg = (
             "Usage: /client.json?platform=android&version=12&nextdns-path=YOUR_PATH\n"
             "Parameters:\n"
-            "- platform: android, ios, or empty (for mac, linux, windows)\n"
-            "- version: 11 for sing-box 1.11+, 12 for sing-box 1.12+\n"
-            "- dns-path: your private NextDNS profile (optional)\n"
-            "- dns-remote-detour: specify any from outbound list, direct will be used if empty\n"
-            "- dns-detour: specify any from outbound list, direct will be used if empty\n"
-            "- dns-final: specify any from dns server list, dns-remote will be used if empty\n"
-            "- route-detour: specify any from outbound list, direct will be used if empty\n"
-            "- log-level: specify any from docs, warn will be used if empty\n"
-            "- ts-auth-key: your Tailscale ephemeral auth key (optional)\n"
-            "- ts-exit-node: your Tailscale exit node IP (optional)\n"
-            "- ts-hostname: your Tailscale hostname (optional, default: ts-sb)\n"
+            "- p: android, ios, or empty for mac, linux, windows\n"
+            "- v: 11 for sing-box 1.11+, 12 for sing-box 1.12+, or empty for 12\n"
+            "- dp: your private NextDNS profile (optional)\n"
+            "- dd: specify any from outbound list, direct will be used if empty\n"
+            "- df: specify any from dns server list, dns-remote will be used if empty\n"
+            "- rdr: local or DNS IP, 1.1.1.1 will be used if empty\n"
+            "- mo: specify any from outbound tags, separated by &\n"
+            "- rd: specify any from outbound list, direct will be used if empty\n"
+            "- ll: specify any from docs, warn will be used if empty\n"
+            "- tak: your Tailscale ephemeral auth key (optional)\n"
+            "- ten: your Tailscale exit node IP (optional)\n"
+            "- th: your Tailscale hostname (optional, default: ts-sb)\n"
         )
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
@@ -56,13 +57,14 @@ class Handler(http.server.BaseHTTPRequestHandler):
                     "address": f"https://dns.nextdns.io/{self.__dns_path}"
                     if self.__dns_path
                     else "https://dns.nextdns.io/",
-                    "address_resolver": "dns-local",
+                    "address_resolver": "dns-resolver",
                     "address_strategy": "ipv4_only",
-                    "detour": self.__dns_remote_detour
+                    "detour": self.__dns_detour
                 },
                 {
-                    "tag": "dns-local",
-                    "address": "local"
+                    "tag": "dns-resolver",
+                    "address": self.__remote_dns_resolver,
+                    "detour": self.__dns_detour
                 }
             ]
         else:
@@ -73,28 +75,30 @@ class Handler(http.server.BaseHTTPRequestHandler):
                     "type": "https",
                     "server": "dns.nextdns.io",
                     "path": self.__dns_path if self.__dns_path else "/",
-                    "domain_resolver": "dns-local",
-                    "detour": self.__dns_remote_detour
+                    "domain_resolver": "dns-resolver",
+                    "detour": self.__dns_detour
                 }
             ]
-            if self.__dns_detour == "direct":
-                # local DNS server with direct detour makes no sense
+            if self.__remote_dns_resolver.lower() == "local":
                 self.remote_data["dns"]["servers"].append({
-                    "tag": "dns-local",
+                    "tag": "dns-resolver",
                     "type": "local"
                 })
             else:
                 self.remote_data["dns"]["servers"].append({
-                    "tag": "dns-local",
-                    "type": "local",
+                    "tag": "dns-resolver",
+                    "type": "udp",
+                    "server": self.__remote_dns_resolver,
                     "detour": self.__dns_detour
                 })
             self.remote_data["route"]["default_domain_resolver"] = {
-                "server": "dns-local",
+                "server": "dns-resolver",
                 "rewrite_ttl": 60,
                 "client_subnet": "1.1.1.1"
             }
         self.remote_data["dns"]["final"] = self.__dns_final
+        # WIFI -> dns-direct --> dns-remote
+        # MOBILE -> dns-local --> dns-remote
 
     def __inject_outbounds(self):
         replaced = []
@@ -189,38 +193,63 @@ class Handler(http.server.BaseHTTPRequestHandler):
         query = urllib.parse.parse_qs(parsed.query)
         path = parsed.path
 
-        if path not in ("/client.json", "/help"):
+        if path not in ("/c", "/h"):
             self.send_response(404)
             self.end_headers()
             self.wfile.write(b"Sorry, Not Found")
             return
 
-        if path == "/help":
+        if path == "/h":
             self.__send_help()
 
         # Otherwise
         # self.__query: dict = query
         # self.__path: str = path
-        self.__platform: list = query.get("platform", [""])[0].split("&")
-        self.__version: str = query.get("version", ["12"])[0]
-        self.__dns_path = query.get("dns-path", [""])[0]
-        self.__dns_remote_detour = query.get("dns-remote-detour", ["direct"])[0]
-        self.__dns_detour = query.get("dns-detour", ["direct"])[0]
-        self.__dns_final = query.get("dns-final", ["dns-remote"])[0]
-        self.__route_detour = query.get("route-detour", ["direct"])[0]
-        self.__log_level = query.get("log-level", ["warn"])[0]
-        self.__modes = query.get("modes", [""])[0].split("&")
 
-        self.__ts_auth_key = query.get("ts-auth-key", [""])[0]
-        self.__ts_exit_node = query.get("ts-exit-node", [""])[0]
-        self.__ts_hostname = query.get("ts-hostname", [""])[0]
+        # Mobile Data
+        # /c?p=a&dp=YOUR_PATH&rdr=local&mo=ss
+        # /cjson?p=i&dp=YOUR_PATH&rdr=local&mo=ss
+
+        # Wi-Fi
+        # /c?p=a&dp=YOUR_PATH&mo=ss&dd=d
+        # /c?p=i&dp=YOUR_PATH&mo=ss&dd=d
+        self.mapping = {"ss": "shadowsocks", "d": "direct", "l": "local"}
+
+        # fmt: off
+        self.__platform: list = query.get("p", [""])[0].split("&")  # p
+        self.__version: str = "12" if self.__platform != "i" else query.get("v", ["12"])[0] # v
+        self.__dns_path = query.get("dp", [""])[0]  # dp
+        self.__dns_final = query.get("df", ["dns-remote"])[0]  # df
+
+        __dd__ = query.get("dd", ["d"])[0] # dd
+        self.__dns_detour = self.mapping.get(__dd__, __dd__)
+
+
+        __rdr__ = query.get("rdr", ["1.1.1.1"])[0] # rdr
+        self.__remote_dns_resolver = self.mapping.get(__rdr__, __rdr__)
+
+        __rd__ = query.get("rd", ["d"])[0]  # rd
+        self.__route_detour = self.mapping.get(__rd__, __rd__)
+
+        __ll__ = query.get("ll", ["warn"])[0]  # ll
+        self.__log_level = self.mapping.get(__ll__, __ll__)
+
+        self.__modes = [
+            self.mapping.get(i, i) for i in query.get("mo", [""])[0].split("&") if i
+        ]  # mo
+
+        self.__ts_auth_key = query.get("tak", [""])[0]  # tak
+        self.__ts_exit_node = query.get("ten", [""])[0]  # ten
+        self.__ts_hostname = query.get("th", [""])[0] or "ts-sb"  # th
+
         # self.__ipv6_only: bool = query.get("ipv6-only", ["false"])[0] == "true"
         # self__enable_ipv6: bool = query.get("enable-ipv6", ["false"])[0] == "true"
+
         self.__load()
         self.__inject_dns()
         self.__inject_outbounds()
         self.__inject_others()
-        if "android" in self.__platform or "ios" in self.__platform:
+        if "a" in self.__platform or "i" in self.__platform:
             self.remote_data["route"]["override_android_vpn"] = True
 
         body = json.dumps(self.remote_data, indent=2).encode()
