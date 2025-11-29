7f58d991c1d1b48e46550d26aa54e4995bcaf45a
minlaxz <minminlaxz@gmail.com>
2025-11-29 23:47:28 +0630

    fix: awk error

--- nekohasekai/init.sh
+++ nekohasekai/init.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 PORT=${START_PORT:-1080}
-SERVER_WG_KEYPAIR=$(/sing-box/sing-box generate wg-keypair) && SERVER_WG_PRIVATE=$(awk 'PrivateKey/{print $NF}' <<< "$SERVER_WG_KEYPAIR") && SERVER_WG_PUBLIC=$(awk 'PublicKey/{print $NF}' <<< "$SERVER_WG_KEYPAIR")
-CLIENT_WG_KEYPAIR=$(/sing-box/sing-box generate wg-keypair) && CLIENT_WG_PRIVATE=$(awk 'PrivateKey/{print $NF}' <<< "$CLIENT_WG_KEYPAIR") && CLIENT_WG_PUBLIC=$(awk 'PublicKey/{print $NF}' <<< "$CLIENT_WG_KEYPAIR")
+SERVER_WG_KEYPAIR=$(/sing-box/sing-box generate wg-keypair)
+SERVER_WG_PRIVATE=$(awk -F': ' '/PrivateKey:/ {print $2}' <<< "$SERVER_WG_KEYPAIR")
+SERVER_WG_PUBLIC=$(awk -F': ' '/PublicKey:/ {print $2}' <<< "$SERVER_WG_KEYPAIR")
+CLIENT_WG_KEYPAIR=$(/sing-box/sing-box generate wg-keypair)
+CLIENT_WG_PRIVATE=$(awk -F': ' '/PrivateKey:/ {print $2}' <<< "$CLIENT_WG_KEYPAIR")
+CLIENT_WG_PUBLIC=$(awk -F': ' '/PublicKey:/ {print $2}' <<< "$CLIENT_WG_KEYPAIR")
 
 python3 ./generator.py \
     --start-port "$PORT" \
