f5becb315960cfe44c042860fe4216e8a6e01d14
minlaxz <minminlaxz@gmail.com>
2026-03-06 18:03:24 +0630

    fix: import

--- api/app/routes/ssm.py
+++ api/app/routes/ssm.py
@@ -11,7 +11,7 @@ from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 
-from api.app.utils import get_stats
+from app.utils import get_stats
 
 router = APIRouter()
 
