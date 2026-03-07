2401b291d0a377a780b58df4a2d4f434de0c4b44
minlaxz <minminlaxz@gmail.com>
2026-03-07 23:41:31 +0630

    feat: User table

--- api/app/routes/ssm.py
+++ api/app/routes/ssm.py
@@ -9,7 +9,7 @@ import json
 from fastapi import APIRouter, HTTPException, Form
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import HTMLResponse
 
 
 from app.utils import get_stats
@@ -24,57 +24,6 @@ APP_SSM_UPSTREAM = os.getenv("APP_SSM_UPSTREAM", "http://sing-box:8888")
 
 
 @router.get(
-    "/server/v1",
-    response_model=Dict[str, List[Dict[str, Any]]],
-    response_class=JSONResponse,
-)
-async def proxy_server_info():
-    async with httpx.AsyncClient(timeout=5) as client:
-        upstream = f"{APP_SSM_UPSTREAM}/server/v1"
-        try:
-            r = await client.get(upstream)
-            r.raise_for_status()
-            return r.json()  # forward upstream JSON to client
-        except httpx.HTTPError as e:
-            raise HTTPException(status_code=502, detail=f"Upstream error: {str(e)}")
-
-
-@router.get(
-    "/server/v1/stats",
-    response_model=Dict[str, List[Dict[str, Any]]],
-    response_class=JSONResponse,
-)
-async def proxy_server_stats():
-    async with httpx.AsyncClient(timeout=5) as client:
-        stats_upstream = f"{APP_SSM_UPSTREAM}/server/v1/stats"
-        try:
-            r = await client.get(stats_upstream)
-            r.raise_for_status()
-            data = r.json()
-            del data["users"]
-            for k, v in data.items():
-                if k.endswith("Bytes"):
-                    if v >= 1 << 30:
-                        data[k] = f"{v / (1 << 30):.2f} GB"
-                    elif v >= 1 << 20:
-                        data[k] = f"{v / (1 << 20):.2f} MB"
-                    elif v >= 1 << 10:
-                        data[k] = f"{v / (1 << 10):.2f} KB"
-                    else:
-                        data[k] = f"{v} B"
-                elif k.endswith("Packets"):
-                    if v >= 1_000_000:
-                        data[k] = f"{v / 1_000_000:.2f} M"
-                    elif v >= 1_000:
-                        data[k] = f"{v / 1_000:.2f} K"
-                    else:
-                        data[k] = f"{v}"
-            return data
-        except httpx.HTTPError as e:
-            raise HTTPException(status_code=502, detail=f"Upstream error: {str(e)}")
-
-
-@router.get(
     "/server/v1/users",
     response_model=Dict[str, List[Dict[str, Any]]],
     response_class=HTMLResponse,
@@ -84,9 +33,11 @@ async def proxy_server_users(
     raw: Optional[bool] = False,
     bar: Optional[bool] = False,
 ):
-    stats: List[Dict[str, Any]] = await get_stats(raw=raw)  # type: ignore
     if raw:
+        stats: List[Dict[str, Any]] = await get_stats(raw=raw)  # type: ignore
         return stats
+
+    stats: List[Dict[str, Any]] = await get_stats()
     templates = Jinja2Templates(directory="templates")
     if bar:
         max_down = max(int(u["downlinkBytes"]) for u in stats)
